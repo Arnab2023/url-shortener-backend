2fb0cecb1017bc9c8d6b2921719cdc4fcf55d8e1
Arnab2023 <arnab192023@gmail.com>
2024-02-07 18:31:42 +0530

    .env file is added and frontend vercel api is connected

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ import DataBaseConnect from "./connect.js";
 import urlRoute from "./routes/url.js";
 import userRoute from "./routes/user.js";
 import { URL } from "./models/url.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const PORT = 8001;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173", ""],
+  origin: [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "url-shortener-frontend-ochre.vercel.app",
+  ],
 };
 
 app.use(cors(corsOptions));
 
-DataBaseConnect(
-  "mongodb+srv://arnab192023:1234@url-short.apfjdje.mongodb.net/short-url"
-).then(() => console.log("Mongodb connected"));
+DataBaseConnect(process.env.MONGO_URI).then(() =>
+  console.log("Mongodb connected")
+);
 
 app.use(express.json());
 app.use("/url", urlRoute);
@@ -44,4 +50,6 @@ app.get("/:shortId", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
+app.listen(PORT|| process.env.PORT, () =>
+  console.log(`Server Started at PORT:${PORT}`)
+);
